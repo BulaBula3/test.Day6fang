309def9e0a2e728193bd883d85a98c905260f7c5
fang <98085603+BulaBula3@users.noreply.github.com>
2022-01-25 20:13:28 +0800

    Add files via upload

---
+++ test.6fang.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main3()
+{
+    int num = 0;
+    do
+    {
+        cout << num << endl;
+        num++;
+    } while (num < 10);
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout << i << endl;
+    }
+
+  
+
+
+    system("pause");
+
+    return 0;
+}
